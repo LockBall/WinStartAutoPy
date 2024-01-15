1d2819a255daa0a283e71207241214f9b00d4bbd
LockBall <johnplutz@gmail.com>
2024-01-14 20:35:52 -0500

    fully functional, check for custom process property name

--- hello_time.bat
+++ hello_time.bat
@@ -1,3 +1,16 @@
 @echo off
 
-python C:\from_git\WinStartAutoPy\hello_time.py %* 
+:: newline
+echo:
+
+echo resetting errorlevel
+ver > nul
+
+echo looking for python_ht process
+tasklist | findstr /I "python_ht.exe"
+if %errorlevel% == 1 (
+    echo starting python_ht script
+    python_ht C:\from_git\WinStartAutoPy\hello_time.py %* 
+) else (
+    echo python_ht already running
+)
